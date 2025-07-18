88a47110eb435922b70e3a929552215f949fcf68
Rinkesh Gorasia <reachrinkesh@gmail.com>
2025-07-18 17:07:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'lexend': ['Lexend Deca', 'sans-serif'],
-        'inter': ['Inter', 'sans-serif'],
-      },
-      colors: {
-        'accent': '#3fa7ff',
-        'accent-tint': '#3fa7ff1a',
-        'surface': '#1a1a1d',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
